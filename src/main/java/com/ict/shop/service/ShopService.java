9dd2c5e1a453be2a0fe73e2589831f4c24fc764a
Haeseongl <has46840@naver.com>
2024-04-06 23:03:44 +0900

    dkkdkd

--- src/main/java/com/ict/shop/service/ShopService.java
+++ src/main/java/com/ict/shop/service/ShopService.java
@@ -10,7 +10,6 @@ import com.ict.shop.dao.vo.UserVO;
 
 public interface ShopService {
 
-	
 //로그인 ============================================================
 	int getShop_Insert(UserVO vo); // 회원가입
 
@@ -21,23 +20,22 @@ public interface ShopService {
 	UserVO find_id(UserVO vo); // 아이디찾기
 
 	UserVO getUser_id(String user_id);
-	
+
 	UserVO find_pwd(UserVO uvo);
 
 	int reset_pwd(UserVO uvo);
-	//마이페이지 =========================================================
-	//mypage_addr
+
+	// 마이페이지 =========================================================
+	// mypage_addr
 	int getAddrInsert(AddrVO vo);
 
 	List<OrderVO> getAddrList(OrderVO ovo);
-	
-	List<AddrVO>getMyAddrList(String user_idx);
 
-	
+	List<AddrVO> getMyAddrList(String user_idx);
+
 //마이페이지 =========================================================
 
-
-	//=======
+	// =======
 	int getAddrDelete(String addr_idx);
 
 	OrderVO getOrderDetail(String order_idx);
@@ -46,66 +44,51 @@ public interface ShopService {
 
 	// 관심상품 리스트
 	public List<OrderVO> getShopHeartList(String user_idx);
-	
+
 	public int getUpdateHeartStatus(OrderVO ovo);
-	
+
 	public int getCartListAdd(OrderVO ovo);
 
 	// 주문 내역 리스트
 	public List<OrderVO> getShopOrderList(String user_idx);
 
 	UserVO firstchk(String user_id);
-	 
 
 	int getChangePwd(UserVO uvo);
 
 	List<OrderVO> getOrderList(String order_idx);
-	
+
 	int getAddrEdit(AddrVO avo);
-	//mypage_addr+order_pay
+
+	// mypage_addr+order_pay
 	AddrVO getAddrDetail(AddrVO avo);
 
 	UserVO getMypage_Info(String user_id);
 
 	int Mypage_Info_Change(UserVO uvo);
 
-
-
-
-
-
-	//오더  ============================================================
+	// 오더 ============================================================
 	// cart list
 	List<CartListVO> getCartList(String user_idx);
-	
+
 	int getCartlistDelete(String cartlist_idx);
-	
+
 	int getCartlistEdit(CartListVO cvo);
-	
+
 	int getCartlistPass(CartListVO cvo2);
-	
+
 	List<CartListVO> getCartlistOrderIdx();
 
 	CartListVO getCartlistSelect(String cartlist_idx);
 
 	CartListVO getCartListAddr(String user_idx);
 
-       //order-pay
+	// order-pay
 
-	List<OrderVO> orderaddrproduct(String order_idx);
-	
-	String orderadd(String order_idx);
+	List<OrderVO> orderaddrproduct(OrderVO ovo);
 
+	int getaddrchecked(AddrVO avo);
 
-
-	
 //프로덕 ============================================================
 
-
-
-
-
-
-
-	 
 }
